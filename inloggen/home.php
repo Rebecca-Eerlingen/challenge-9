3bca5c5aa74b5a3d9e81c55aa6067e13f2551b7f
Rebecca-Eerlingen <519561@Vistacollege.nl>
2026-05-11 09:06:06 +0200

    Create home.php

---
+++ inloggen/home.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>homepage</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+</head>
+<body>
+    <div class="navbar">
+        <div class="logo">spik & span</div>
+    </div>
+
+    <div class="reserveren">
+        <img id="spik-span-logo" src="logo.png">
+        <p> Welkom bij spik & span! </p>
+        <a class="ticket-knop" href="login.php"> Reserveer uw ticket </a>
+    </div>
+    
+</body>
+</html>
