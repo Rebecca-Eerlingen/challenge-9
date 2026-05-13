fc0b7abb7db210c7f46f0a4e8ec5b97ace9c7eee
Catharina <catharina@simenon.cc>
2026-05-13 13:02:47 +0200

    Add files via upload

---
+++ Startpagina/HomePage.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage - Spik&Span</title>
+    <link rel="stylesheet" type="text/css" href="login.css">
+        <link rel="stylesheet" href=".js">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <section class="header">
+        <a href="login.php"><img src="LogoSpikSpan"></a>
+        <div class="nav-links" id="navLinks">
+            <i class="fa fa-times" onclick="hideMenu()"></i>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">PAGE 1...</a></li>
+                <li><a href="">PAGE 2...</a></li>
+                <li><a href="">PAGE 3...</a></li>
+            </ul>
+        </div>
+        <i class="fa fa-bars" onclick="showMenu()"></i>
+    </nav> 
+<div class="text-box">
+        <h1>Spik & Span</h1>
+        <p>Jo Huijnen en Niek Dirkx, oftewel Spik en Span. De Kampioene van de Nach!</p>
+        <a href="inlog.php" class="ticket-knop">Reserveer uw ticket</a>
+</div>
+    </section>
+
+    
+</body>
+</html>
+
+
